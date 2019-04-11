347b4e2c10691d17ceaa0c7a1f023f50b0881d36
Haina <43018326+Arrondissement5etDemi@users.noreply.github.com>
2019-04-11 18:49:32 +0100

    Update LjPotential.java

--- LjPotential.java
+++ LjPotential.java
@@ -1,7 +1,7 @@
 import java.util.*;
 /**
 A class that computes the Lennard-Jones potential
-@author Haina Wang (Studio-Darboux-Carbonnier)
+@author Haina Wang (Arrondissement5etDemi)
 */
 
 public class LjPotential {
@@ -10,7 +10,7 @@ public class LjPotential {
 
 	/** computes the LJ-potential given r, r0, e (see parameter descriptions)
  	@param r double, the internuclear distance variable
-	@param r0 double, the minimum-energy internuclear distance
+	@param r0 double, the internuclear distance where the energy is 0
 	@param e double, the depth of the energy well
 	@return double, the LJ potential at internuclear distance r */
 	public static double ljClassic(double r, double r0, double e) {
@@ -33,7 +33,7 @@ public class LjPotential {
 		if (r >= rc) {
 			return 0;
 		}
-		/** double uprc = 0.0389995; //u'(2.5), the derivative at rc*/
+		/** double uprc = 0.0389995; =u'(2.5), the derivative at rc, sometimes useful*/
 		double result = lj(r) - urc;
 		return result;
 	}
